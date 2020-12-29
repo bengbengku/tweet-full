12932ffd0f203e3b0ed91de3093448ccd0a19d55
bengbengku <anggiatbenget70@gmail.com>
2020-12-29 22:33:01 +0700

    ADD: "Fix like dan unlike button"

--- public/js/common.js
+++ public/js/common.js
@@ -39,7 +39,9 @@ $(document).on("click", ".likeButton", (event) => {
     url: `/api/posts/${postId}/like`,
     type: "PUT",
     success: (postData) => {
-      console.log(postData);
+      
+      button.find("span").text(postData.likes.length || "");
+
     },
   });
 });
@@ -93,6 +95,7 @@ function createPostHtml(postData) {
                         <div class="postButtonContainer">
                             <button class="likeButton">
                                 <i class="far fa-heart"></i>
+                                <span>${postData.likes.length || ""}</span>
                             </button>
                         </div>
                     </div>
