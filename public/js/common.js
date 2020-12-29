0228261ddea2aaf241135ebc2a772ffa66975093
bengbengku <anggiatbenget70@gmail.com>
2020-12-29 18:43:10 +0700

    ADD: "Penerapan fungsi perbedaan waktu pada timestamps, dll"

--- public/js/common.js
+++ public/js/common.js
@@ -1,38 +1,43 @@
 $("#postTextArea").keyup((event) => {
-  var textbox = $(event.target)
-  var value = textbox.val().trim()
+  var textbox = $(event.target);
+  var value = textbox.val().trim();
 
-  var submitButton = $("#submitPostButton")
-  if (submitButton.length == 0) return alert("No submit button found")
+  var submitButton = $("#submitPostButton");
+  if (submitButton.length == 0) return alert("No submit button found");
 
   if (value == "") {
-    submitButton.prop("disabled", true)
-    return
+    submitButton.prop("disabled", true);
+    return;
   }
 
-  submitButton.prop("disabled", false)
-})
+  submitButton.prop("disabled", false);
+});
 
 $("#submitPostButton").click(() => {
-  var button = $(event.target)
-  var textbox = $("#postTextArea")
+  var button = $(event.target);
+  var textbox = $("#postTextArea");
 
   var data = {
     content: textbox.val(),
-  }
+  };
 
   $.post("/api/posts", data, (postData) => {
-    var html = createPostHtml(postData)
-    $(".postContainer").prepend(html)
-    textbox.val("")
-    button.prop("disabled", true)
-  })
-})
+    var html = createPostHtml(postData);
+    $(".postContainer").prepend(html);
+    textbox.val("");
+    button.prop("disabled", true);
+  });
+});
 
 function createPostHtml(postData) {
   var postedBy = postData.postedBy;
+
+  if (postedBy._id === undefined) {
+    return console.log("user not populated!");
+  }
+
   var displayNama = postedBy.firstName + " " + postedBy.lastName;
-  var timestamp = "2 days ago"
+  var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
 
   return `
         <div class="post">
@@ -50,10 +55,51 @@ function createPostHtml(postData) {
                         <span>${postData.content}</span>
                     </div>
                     <div class="postFooter">
-                        
+                        <div class="postButtonContainer">
+                            <button>
+                                <i class="far fa-comments"></i>
+                            </button>
+                        </div>
+                        <div class="postButtonContainer">
+                            <button>
+                                <i class="fas fa-retweet"></i>
+                            </button>
+                        </div>
+                        <div class="postButtonContainer">
+                            <button>
+                                <i class="far fa-heart"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    `
+    `;
+}
+
+function timeDifference(current, previous) {
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  var msPerYear = msPerDay * 365;
+
+  var elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    if (elapsed / 1000 < 30) {
+      return "Just now";
+    }
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return Math.round(elapsed / msPerYear) + " years ago";
+  }
 }
---
+++ public/js/home.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+    $.get("/api/posts", results => {
+      
+        outputPosts(results, $(".postContainer"));
+
+    })
+})
+
+function outputPosts(results, container) {
+    container.html("");
+
+    results.forEach(result => {
+        var html = createPostHtml(result)
+        container.append(html);
+    });
+
+    if(results.length == 0) {
+        container.append("<span class='noResult'>Nothing to show!</span>")
+    }
+}
