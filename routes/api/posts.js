722834a47612476ac430bd2223c46dd127baf3b4
bengbengku <anggiatbenget70@gmail.com>
2021-01-03 16:36:32 +0700

    ADD: "Mengirim data following/followers ke mongoDB"

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,7 +15,6 @@ router.get("/", async (req, res, next) => {
         var isReply = searchObj.isReply == "true";
         searchObj.replyTo = {$exists: isReply}
         delete searchObj.isReply;
-        console.log(searchObj);
     }
 
     var results = await getPosts(searchObj);
--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,28 @@ router.put("/:userId/follow", async (req, res, next) => {
   var userId = req.params.userId;
   
   var user = await User.findById(userId);
-  
+
+  if(user == null) return res.sendStatus(404);
+
+  var isFollowing = user.followers && user.followers.includes(req.session.user._id);
+  var option = isFollowing ? "$pull" : "$addToSet";
+
+  req.session.user = await User.findByIdAndUpdate(
+    req.session.user._id,
+    { [option]: { following: userId } },
+    { new: true }
+  ).catch((error) => {
+    console.log(error);
+    res.sendStatus(400);
+  });
+
+ User.findByIdAndUpdate(userId, {
+   [option]: { followers: req.session.user._id },
+ }).catch((error) => {
+   console.log(error);
+   res.sendStatus(400);
+ });
+  res.status(200).send(req.session.user);
 });
 
 
