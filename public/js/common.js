ee5dc4df47f2878e9d2f0ea5997999af4d62be36
bengbengku <anggiatbenget70@gmail.com>
2021-01-06 22:58:50 +0700

    ADD: "Cropper penerapan"

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,8 @@
+//Global variable untuk cropper upload picture
+
+var cropper;
+
+
 $("#postTextarea, #replyTextarea").keyup((event) => {
   var textbox = $(event.target);
   var value = textbox.val().trim();
@@ -80,6 +85,27 @@ $("#deletePostButton").click((event) => {
     },
   });
 });
+
+$("#filePhoto").change(function(){  
+  if(this.files && this.files[0]) {
+    var reader = new FileReader();
+    reader.onload = (e) => {
+
+      var image = document.getElementById("imagePreview");
+      image.src = e.target.result;
+
+      if(cropper !== undefined) {
+        cropper.destroy();
+      }
+
+      cropper = new Cropper(image, {
+        aspectRatio: 1/1,
+        background: false
+      });
+    }
+    reader.readAsDataURL(this.files[0]);
+  }
+})
 
 $(document).on("click", ".likeButton", (event) => {
   var button = $(event.target);
