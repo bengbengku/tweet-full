8100c368709660fceabc1dc55c70c522b3bebdfc
bengbengku <anggiatbenget70@gmail.com>
2021-01-09 18:27:42 +0700

    ADD: "Styling border Pin dan Penerapan fungsi unpin modal"

--- public/js/common.js
+++ public/js/common.js
@@ -75,6 +75,12 @@ $("#confirmPinModal").on("show.bs.modal", (event) => {
   $("#pinPostButton").data("id", postId);
 });
 
+$("#unpinModal").on("show.bs.modal", (event) => {
+  var button = $(event.relatedTarget);
+  var postId = getPostIdFromElement(button);
+  $("#unpinPostButton").data("id", postId);
+});
+
 $("#deletePostButton").click((event) => {
   var postId = $(event.target).data("id");
 
@@ -99,6 +105,24 @@ $("#pinPostButton").click((event) => {
     url: `/api/posts/${postId}`,
     type: "PUT",
     data: { pinned: true },
+    success: (data, status, xhr) => {
+      if (xhr.status != 204) {
+        alert("could not delete post");
+        return;
+      }
+
+      location.reload();
+    },
+  });
+});
+
+$("#unpinPostButton").click((event) => {
+  var postId = $(event.target).data("id");
+
+  $.ajax({
+    url: `/api/posts/${postId}`,
+    type: "PUT",
+    data: { pinned: false },
     success: (data, status, xhr) => {
       if (xhr.status != 204) {
         alert("could not delete post");
@@ -348,13 +372,15 @@ function createPostHtml(postData, largeFont = false) {
   if (postData.postedBy._id == userLoggedIn._id) {
 
     var pinnedClass = "";
+    var dataTarget = "#confirmPinModal";;
     if(postData.pinned === true) {
+      dataTarget = "#unpinModal";
       pinnedClass = "active";
       pinnedPostText = "<i class='fas fa-thumbtack'></i> <span>Pinned post</span>";
     }
 
     buttons = `
-      <button class='pinButton ${pinnedClass}' data-id="${postData._id}" data-toggle="modal" data-target="#confirmPinModal"><i class='fas fa-thumbtack'></i></button>
+      <button class='pinButton ${pinnedClass}' data-id="${postData._id}" data-toggle="modal" data-target="${dataTarget}"><i class='fas fa-thumbtack'></i></button>
       <button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='fas fa-times'></i></button>
     `;
   }
