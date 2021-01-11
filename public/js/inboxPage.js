363790fbdec094d679131382326cedf988b66a24
bengbengku <anggiatbenget70@gmail.com>
2021-01-12 00:21:25 +0700

    ADD: "Routing mendapatkan results chats"

---
+++ public/js/inboxPage.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $.get("/api/chats", (data, status, xhr) => {
+        if(xhr.status == 400) {
+            alert("Could not get chat list.");
+        }
+        else {
+            outputChatList(data, $(".resultsContainer"));
+        }
+    })
+})
+
+function outputChatList(chatList, container) {
+    console.log(chatList);
+}
