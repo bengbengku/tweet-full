591de2d73b81f8f3bfac27ff1aa1fb234defc149
bengbengku <anggiatbenget70@gmail.com>
2021-01-14 00:06:59 +0700

    ADD: "setup ajax call pada penggantian nama chat"

---
+++ public/js/chatPage.js
@@ -0,0 +1,18 @@
+$("#chatNameButton").click(() => {
+    var name = $("#chatNameTextbox").val().trim();
+    
+    
+    $.ajax({
+        url: "/api/chat/" + chatId,
+        type: "PUT",
+        data: { chatName: name },
+        success: (data, status, xhr) => {
+            if(xhr.status != 204) {
+                alert("Could not updated");
+            }
+            else {
+                location.reload();
+            }
+        }
+    })
+});
