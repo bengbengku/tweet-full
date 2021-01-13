a35c853641da53aa37c901673bffa4daced08c5a
bengbengku <anggiatbenget70@gmail.com>
2021-01-14 01:11:57 +0700

    ADD: "Setup chat submit handlers"

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,25 +1,37 @@
 $(document).ready(() => {
-    $.get(`/api/chats/${chatId}`, (data) => {
-        $("#chatName").text(getChatName(data));
-    })
-})
-
-$("#chatNameButton").click(() => {
-    var name = $("#chatNameTextbox").val().trim();
-    
-    
-    $.ajax({
-        url: "/api/chats/" + chatId,
-        type: "PUT",
-        data: { chatName: name },
-        success: (data, status, xhr) => {
-            if(xhr.status != 204) {
-                alert("Could not updated");
-            }
-            else {
-                location.reload();
-            }
-        }
-    })
+  $.get(`/api/chats/${chatId}`, (data) => {
+    $("#chatName").text(getChatName(data));
+  });
 });
 
+$("#chatNameButton").click(() => {
+  var name = $("#chatNameTextbox").val().trim();
+
+  $.ajax({
+    url: "/api/chats/" + chatId,
+    type: "PUT",
+    data: { chatName: name },
+    success: (data, status, xhr) => {
+      if (xhr.status != 204) {
+        alert("Could not updated");
+      } else {
+        location.reload();
+      }
+    },
+  });
+});
+
+$(".sendMessageButton").click(() => {
+  messageSubmitted();
+});
+
+$(".inputTextbox").keydown((event) => {
+  if (event.which === 13 && !event.shiftKey) {
+    messageSubmitted();
+    return false;
+  }
+});
+
+function messageSubmitted() {
+  console.log("Submitted");
+}
