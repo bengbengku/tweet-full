7d22f0258b71f1c6628cdbfe1afe8692293c9c2f
bengbengku <anggiatbenget70@gmail.com>
2021-01-15 05:05:38 +0700

    ADD: "Message content html setup"

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -43,6 +43,37 @@ function messageSubmitted() {
 
 function sendMessage(content) {
   $.post("/api/messages", { content: content, chatId: chatId }, (data, status, xhr) => {
-    console.log(data);
-  })
+    addChatMessageHtml(data);
+  });
+
+}
+
+function addChatMessageHtml(message) {
+  if(!message || !message._id) {
+    alert("Message not valid!");
+  }
+
+  var messageDiv = createMessageHtml(message);
+
+  $(".chatMessages").append(messageDiv);
+
+
+}
+
+function createMessageHtml(message) {
+
+  var isMine = message.sender._id == userLoggedIn._id;
+  var liClassname = isMine ? "mine" : "theirs";
+
+  return `
+    <li class='message ${liClassname}'>
+      <div class='messageContainer'>
+        <span class='messageBody'>
+          ${message.content}
+        </span>
+      </div>
+    </li>
+  `;
+
+
 }
