e6a7ffc16257a09f118af64ddc8a16f364974066
bengbengku <anggiatbenget70@gmail.com>
2021-01-15 04:47:36 +0700

    ADD: "MessageSchema dan mengirim data message ke mongoDB"

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ const messagesRoute = require('./routes/messagesRoutes');
 const postsApiRoute = require('./routes/api/posts');
 const usersApiRoute = require('./routes/api/users');
 const chatsApiRoute = require('./routes/api/chats');
+const messagesApiRoute = require('./routes/api/messages');
 
 app.use("/login", loginRoute);
 app.use("/logout", logoutRoute);
@@ -50,6 +51,7 @@ app.use("/messages", middleware.requireLogin, messagesRoute);
 app.use("/api/posts", postsApiRoute);
 app.use("/api/users", usersApiRoute);
 app.use("/api/chats", chatsApiRoute);
+app.use("/api/messages", messagesApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
